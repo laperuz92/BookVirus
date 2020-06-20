ee69afe37f7add4e811dda871a013cb6f0cebe82
Laperuz92 <laperuz@sibnet.ru>
2020-06-20 13:51:51 +0800

    Ready for github pages

--- src/App.js
+++ src/App.js
@@ -1,44 +1,65 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import {Cell, Group, List, Panel, PanelHeader, View} from '@vkontakte/vkui';
 import bridge from '@vkontakte/vk-bridge';
-import View from '@vkontakte/vkui/dist/components/View/View';
-import ScreenSpinner from '@vkontakte/vkui/dist/components/ScreenSpinner/ScreenSpinner';
 import '@vkontakte/vkui/dist/vkui.css';
 
-import Home from './panels/Home';
-import Persik from './panels/Persik';
-
-const App = () => {
-	const [activePanel, setActivePanel] = useState('home');
-	const [fetchedUser, setUser] = useState(null);
-	const [popout, setPopout] = useState(<ScreenSpinner size='large' />);
-
-	useEffect(() => {
-		bridge.subscribe(({ detail: { type, data }}) => {
-			if (type === 'VKWebAppUpdateConfig') {
-				const schemeAttribute = document.createAttribute('scheme');
-				schemeAttribute.value = data.scheme ? data.scheme : 'client_light';
-				document.body.attributes.setNamedItem(schemeAttribute);
-			}
-		});
-		async function fetchData() {
-			const user = await bridge.send('VKWebAppGetUserInfo');
-			setUser(user);
-			setPopout(null);
-		}
-		fetchData();
-	}, []);
-
-	const go = e => {
-		setActivePanel(e.currentTarget.dataset.to);
-	};
-
-	return (
-		<View activePanel={activePanel} popout={popout}>
-			<Home id='home' fetchedUser={fetchedUser} go={go} />
-			<Persik id='persik' go={go} />
-		</View>
-	);
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        
+        this.invintationID = "";
+    }
+    
+    componentDidMount() {
+        bridge.subscribe((e) => {
+            switch (e.detail.type) {
+                case 'VKWebAppStorageGetResult':
+                    this.getStoredInvitationKey(e.detail.data.keys);
+                    break;
+                case 'VKWebAppStorageSetResult':
+                    break;
+                default:
+                    console.log(e.detail.type);
+            }
+        });
+        bridge.send('VKWebAppStorageGet', {"keys": ["invitationID"]});
+    }
+      
+    getInvitationID = (string) => {
+        let linkParams = string.split("&");
+        
+        for(var i=0; i < linkParams.length; i++){
+            let value = linkParams[i].split("=");
+            if (value[0] === "id"){
+                return value[1];
+            }
+        }
+        return "";
+    }
+    
+    getStoredInvitationKey = (string) => {
+        let data = JSON.parse(string);
+        this.invintationID = data["invintationID"];
+    }
+    
+    saveInvintationID = (string) => {
+        bridge.send("VKWebAppStorageSet", {"key": "invitationID", "value": string});
+    }
+    
+    render() {
+        const invintationID = this.getInvitationID(window.location.search.substring(1));
+        
+        if (this.invintationID === "" && invintationID != ""){
+            this.invintationID = invintationID;
+            this.saveInvintationID(invintationID);
+        }
+        
+        if(invintationID === ""){
+            return (<h1>{this.invintationID}</h1>)
+        }else{
+                return (<h2>{this.invintationID}</h2>)
+        }
+    }
 }
 
 export default App;
-
